55de2f31eadf382561886f7ba452a2123a638663
Pahan Supurna <pahansupurna2006@gmail.com>
2024-12-06 23:26:00 +0530

    Initial commit of the backend

---
+++ src/main/java/com/w2052962/ticket_booking_system_backend/TicketBookingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.w2052962.ticket_booking_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketBookingSystemBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/w2052962/ticket_booking_system_backend/TicketBookingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2052962.ticket_booking_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
